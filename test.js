5302e3a013c7f6a32cd0fce07ef9cc1614ad07da
Logan Miller <Zogzmiller@gmail.com>
2018-10-17 20:41:25 -0500

    test

--- test.js
+++ test.js
@@ -84,8 +84,30 @@ function bindButtons(){
                     var data = [];
                     data.push(response.petfinder.shelters.shelter);
                     data.forEach(function(dataParsing) {
-                        Object.entries(dataParsing).forEach(function([key, value]) {
-                            console.log(value.city.$t);
+                        Object.entries(dataParsing).forEach(function([key, value]) {                         
+                            let id = value.id.$t;
+                            let name = value.name.$t;
+                            let phone = value.phone.$t;
+                            let address = value.address1.$t;
+                            let city = value.city.$t;
+                            let state = value.state.$t;
+                            let country = value.country.$t;
+                            let zip = value.zip.$t;
+                            let email = value.email.$t;
+                            let latitude = value.latitude.$t;
+                            let longitude = value.longitude.$t;
+                            let newDiv = body.append("li");
+                            newDiv.text(`name: ${name}^
+                            id: ${id}^
+                            phone: ${phone}^
+                            address: ${address}^
+                            city: ${city}^
+                            state: ${state}^
+                            country: ${country}^
+                            zip: ${zip}^
+                            email: ${email}^
+                            latitude: ${latitude}^
+                            longitude: ${longitude}^`);
                         });
                     });
                 }
