49a402e76a03da1a62db94c415a81db7522b1b74
Logan Miller <Zogzmiller@gmail.com>
2018-10-17 19:01:20 -0500

    test

--- test.js
+++ test.js
@@ -54,7 +54,6 @@
 //   .error(function(err) { alert('Error retrieving data!'); 
 // });
 // 96701, 99703, 30301, 83701, 66212, 10001, 60007, 80014, 90001, 58501
-var shelterIds = []
 document.addEventListener('DOMContentLoaded', bindButtons);
 // var url = 'https://api.petfinder.com/shelter.find';
 var apiKey = "0d60940cc1ebb2a47f2f53bd4ec5f607"	
@@ -65,7 +64,7 @@ var body = d3.select("body")
 function bindButtons(){
     document.getElementById('submitZip').addEventListener('click', function(event){
         event.preventDefault();
-        // var dropdown = document.getElementById('animus').value;
+        var dropdown = document.getElementById('animus').value;
         var zip = [58501]; // this line gets the zip code from the form entry
         var url = 'https://api.petfinder.com/pet.find';
         zip.forEach(function(zipper){
@@ -75,7 +74,7 @@ function bindButtons(){
                 dataType: "jsonp",
                 data: {
                     key: apiKey,
-                    animal: "Dogs",
+                    animal: dropdown,
                     'location': zipper,
                     output: 'basic',
                     count: 10,
