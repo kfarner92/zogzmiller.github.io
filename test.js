db97384fe1bb1149396691e05a4cd0fb83334808
Logan Miller <Zogzmiller@gmail.com>
2018-10-17 19:34:38 -0500

    test

--- test.js
+++ test.js
@@ -86,7 +86,7 @@ function bindButtons(){
                     data.push(response.petfinder.pets.pet);
                     data.forEach(function(dataParsing) {
                         Object.entries(dataParsing).forEach(function([key, value]) {
-                            let breeds = value.breeds.breed.$t;
+                            let breeds = value.breeds;
                             console.log(breeds)
                         });
                     });
