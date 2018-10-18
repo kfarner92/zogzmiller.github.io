81becc58082e26b81d996be9ac2431255b9be0c5
Logan Miller <Zogzmiller@gmail.com>
2018-10-17 20:48:53 -0500

    test

--- test.js
+++ test.js
@@ -65,7 +65,7 @@ function bindButtons(){
     document.getElementById('submitZip').addEventListener('click', function(event){
         event.preventDefault();
         var dropdown = document.getElementById('animus').value;
-        var zip = [30301]; // this line gets the zip code from the form entry
+        var zip = [83701]; // this line gets the zip code from the form entry
         var url = 'https://api.petfinder.com/shelter.find';
         zip.forEach(function(zipper){
             $.ajax({
