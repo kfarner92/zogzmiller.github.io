9ca2a89aa57b412764b3efec7be700aeab0a5538
Logan Miller <Zogzmiller@gmail.com>
2018-10-20 11:48:14 -0500

    test

--- test2.js
+++ test2.js
@@ -29,7 +29,7 @@ function bindButtons(){
                 data.push(response.petfinder.pets.pet);
                 data.forEach(function(dataParsing) {
                     Object.entries(dataParsing).forEach(function([key, value]) {
-                        console.log(value.breeds.breeds.$t);
+                        console.log(value.breeds.$t);
                         let breeds = []
                         var breed1;
                         var breed2;
