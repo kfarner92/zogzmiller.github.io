9939986db958063e726ba2fb1c2f7c9dd91798fd
Logan Miller <Zogzmiller@gmail.com>
2018-10-21 13:26:38 -0500

    test

--- backEnd/animaltest.py
+++ backEnd/animaltest.py
@@ -15,7 +15,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 conn = sqlite3.connect('petfinder.db') 
 c = conn.cursor()
-# c.execute('''CREATE TABLE dogs
+# c.execute('''CREATE TABLE cats
 #         (id text, name text,age text, breed text, animal text, shelterId text, sex text, website text)''')
 # c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
 zippers = [90001,90211,90608]
@@ -31,19 +31,19 @@ for z in zippers:
     listings = browser.find_by_tag('li')
 
     for i in listings:
-            dogs = []
+            cats = []
             text = i.text.split('^ ')
             for x in range(8):
                     value = text[x].split(': ')[1]
                     if '"' in value:
                             standin = 'Doug Funny'
-                            dogs.append(standin)
+                            cats.append(standin)
                     else:
-                            dogs.append(value)
-            # c.executemany('INSERT INTO dogs VALUES (?,?,?,?,?,?,?)', dogs)
-            c.execute(f'INSERT INTO dogs VALUES ("{dogs[0]}","{dogs[1]}","{dogs[2]}","{dogs[3]}","{dogs[4]}","{dogs[5]}","{dogs[6]}","{dogs[7]}")')
-    # c.execute(f"INSERT INTO dogs VALUES ('{dogs}')")
-# c.executemany('INSERT INTO dogs VALUES (?,?,?,?,?,?,?)', dogs)
+                            cats.append(value)
+            # c.executemany('INSERT INTO cats VALUES (?,?,?,?,?,?,?)', cats)
+            c.execute(f'INSERT INTO cats VALUES ("{cats[0]}","{cats[1]}","{cats[2]}","{cats[3]}","{cats[4]}","{cats[5]}","{cats[6]}","{cats[7]}")')
+    # c.execute(f"INSERT INTO cats VALUES ('{cats}')")
+# c.executemany('INSERT INTO cats VALUES (?,?,?,?,?,?,?)', cats)
 
     conn.commit()
 
