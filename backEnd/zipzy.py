de63d3f40d4037eb0f8b7662a1ca15c18702128c
Eljay <eljay@Eljays-MacBook-Pro.local>
2018-11-30 00:49:12 -0600

    test

--- backEnd/zipzy.py
+++
@@ -1,7 +0,0 @@
-data = open("states.txt", "r")
-for state in data:
-    state = state.split(",")
-    print(state)
-    
-    newdata = open("newstates.txt", "a+")
-    newdata.write(state[0]+ f"\n")
--- web 2/config.js
+++
@@ -1,2 +0,0 @@
-// API Key
-const API_KEY = "pk.eyJ1Ijoic2ltcHNvbjEyMDEzIiwiYSI6ImNqbXRzb21lcDAwZGszbXM2Y2x0aHlmbXgifQ.jsc4bXY0j-pu_mZQchdj3A"
